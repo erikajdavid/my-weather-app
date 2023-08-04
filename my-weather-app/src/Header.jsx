7b97a1b12988d8f3b5316ef28fb3f01bf72fdcd3
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-04 14:27:18 -0400

    updated header h2

--- my-weather-app/src/Header.jsx
+++ my-weather-app/src/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ setCity, searchCity }) => {
   return (
     <header>
       <h1>My basic-ass weather app</h1>
-      <h2>A simple daily weather app that is nowhere near as complex as the one you're already using.</h2>
+      <h2>A real-time weather app that is nowhere near as complex as the one you're already using.</h2>
       <SearchBar setCity={setCity} searchCity={searchCity} />
     </header>
   )
