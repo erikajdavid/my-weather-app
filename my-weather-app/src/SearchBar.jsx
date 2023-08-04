bee6968e321fc1b1dc106d5a6689977407a56de3
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-04 14:26:06 -0400

    updated header and search bar styling

--- my-weather-app/src/SearchBar.jsx
+++ my-weather-app/src/SearchBar.jsx
@@ -13,7 +13,7 @@ const SearchBar = ({ setCity, searchCity }) => {
   };
 
   return (
-    <div>
+    <div className="inputSearchBar">
       <input
         type="text"
         placeholder="Enter your city"
@@ -21,7 +21,7 @@ const SearchBar = ({ setCity, searchCity }) => {
         onChange={handleInputChange}
       />
       <button onClick={handleSearchClick} type="submit">
-        Find
+        Go
       </button>
     </div>
   );
