33c8b80a44f396c22956ad9f70e8972412cb40a1
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-04 00:03:39 -0400

    refractored code

--- my-weather-app/src/WeatherData.jsx
+++ my-weather-app/src/WeatherData.jsx
@@ -11,22 +11,39 @@ const WeatherData = ({ weather }) => {
       <div className="AppContainer">
          <p className="city">{weather.name}</p>
             {weather.main && ( // Only render the details if weather.main is available
-                <>
-                <h2 className="temp">{Math.round(weather.main.temp)}°C</h2>
-                <img
-                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-                  alt="Weather Icon"
-                />
-                <p className="description">{weather.weather[0].main}</p>
-                <p className="feelsLike">Feels like: {Math.round(weather.main.feels_like)}°C</p>
+          <>
+            <div className="mainMaxMinTempContainer">
+              <h2 className="temp">{Math.round(weather.main.temp)}°C</h2>
+              <div className="maxMinTempContainer">
                 <p className="maxTemp">H: {Math.round(weather.main.temp_max)}°C</p>
                 <p className="minTemp">L: {Math.round(weather.main.temp_min)}°C</p>
-                <p className="humidity">Humidity: {weather.main.humidity}%</p>
-                <p className="pressure">Pressure: {formatPressure(weather.main.pressure)} hPa</p>
-                <p className="windSpeed">Wind Speed: {weather.wind.speed} km/h</p>
-                </>
-            )}
-        </div>
+              </div>
+            </div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+              alt="Weather Icon"
+            />
+            <div className="bottomHalfContainer">
+              <p className="description">{weather.weather[0].main}</p>
+              <p className="feelsLikeTemp">Feels like: {Math.round(weather.main.feels_like)}°C</p>
+              <div class="extras">
+                <div className="extraInfoContainer">
+                  <p className="extraInfoTitle">Wind Speed:</p> 
+                  <p className="extraInfoDescription">{weather.wind.speed} km/h</p>
+                </div>
+                <div className="extraInfoContainer">
+                  <p className="extraInfoTitle">Humidity:</p> 
+                  <p className="extraInfoDescription">{weather.main.humidity}%</p>
+                </div>
+                <div className="extraInfoContainer">
+                  <p className="extraInfoTitle">Pressure:</p> 
+                  <p className="extraInfoDescription">{formatPressure(weather.main.pressure)} hPa</p>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </>
   )
 }
