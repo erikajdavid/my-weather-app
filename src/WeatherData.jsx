e78297b8058e56e133cb02ba7b84f6953bc05616
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-06 17:17:01 -0400

    added and styled a placeholder img on page load/refresh

--- src/WeatherData.jsx
+++ src/WeatherData.jsx
@@ -15,6 +15,7 @@ import unclearNightIcon from './myIcons/50n.png';
 import windIcon from './myIcons/wind.png';
 import humidityIcon from './myIcons/humidity.png';
 import pressureIcon from './myIcons/pressure.png';
+import placeholderImg from './myicons/placeholderImg.png';
 
 const WeatherData = ({ weather }) => {
   let [weatherIconUrl, setWeatherIconUrl] = useState('');
@@ -90,41 +91,49 @@ const WeatherData = ({ weather }) => {
   return (
     <>
       <div className="AppContainer">
-        <p className="city">{weather.name}</p>
-        {weather.main && (
+        {!weather.main ? (
+          <div className="placeholderImgContainer">
+            <img className="placeholderImg" src={placeholderImg} alt="Sun and clouds" />
+          </div>
+        ) : (
           <>
-            <p className="dateAndTime">{ localTime }</p>
-            <div className="mainMaxMinTempContainer">
-              <h2 className="temp">{Math.round(weather.main.temp)}°C</h2>
-              <div className="maxMinTempContainer">
-                <p className="maxTemp">H: {Math.round(weather.main.temp_max)}°C</p>
-                <p className="minTemp">L: {Math.round(weather.main.temp_min)}°C</p>
-              </div>
-            </div>
-            {weatherIconUrl && ( // Check if the weatherIconUrl is available before rendering the icon
-              <img className="tempIcon" src={weatherIconUrl} alt="Weather Icon" />
+            <p className="city">{weather.name}</p>
+            {weather.main && (
+              <>
+                <p className="dateAndTime">{localTime}</p>
+                <div className="mainMaxMinTempContainer">
+                  <h2 className="temp">{Math.round(weather.main.temp)}°C</h2>
+                  <div className="maxMinTempContainer">
+                    <p className="maxTemp">H: {Math.round(weather.main.temp_max)}°C</p>
+                    <p className="minTemp">L: {Math.round(weather.main.temp_min)}°C</p>
+                  </div>
+                </div>
+                {weatherIconUrl && (
+                  <img className="tempIcon" src={weatherIconUrl} alt="Weather Icon" />
+                )}
+                <div className="bottomHalfContainer">
+                  <p className="description">{weather.weather[0].main}</p>
+                  <p className="feelsLikeTemp">Feels like: {Math.round(weather.main.feels_like)}°C</p>
+                  <div className="extras">
+                    <div className="extraInfoContainer">
+                      <p className="extraInfoTitle">Wind Speed:</p>
+                      <img className="miniIcon" src={windIcon} alt="wind icon" />
+                      <p className="extraInfoDescription">{weather.wind.speed} km/h</p>
+                    </div>
+                    <div className="extraInfoContainer">
+                      <p className="extraInfoTitle">Humidity:</p>
+                      <img className="miniIcon" src={humidityIcon} alt="humidity icon" />
+                      <p className="extraInfoDescription">{weather.main.humidity}%</p>
+                    </div>
+                    <div className="extraInfoContainer">
+                      <p className="extraInfoTitle">Pressure:</p>
+                      <img className="miniIcon" src={pressureIcon} alt="hpa icon" />
+                      <p className="extraInfoDescription">{formatPressure(weather.main.pressure)} hPa</p>
+                    </div>
+                  </div>
+                </div>
+              </>
             )}
-            <div className="bottomHalfContainer">
-              <p className="description">{weather.weather[0].main}</p>
-              <p className="feelsLikeTemp">Feels like: {Math.round(weather.main.feels_like)}°C</p>
-              <div className="extras">
-                <div className="extraInfoContainer">
-                  <p className="extraInfoTitle">Wind Speed:</p> 
-                  <img className="miniIcon"src={windIcon} alt="wind icon"></img>
-                  <p className="extraInfoDescription">{weather.wind.speed} km/h</p>
-                </div>
-                <div className="extraInfoContainer">
-                  <p className="extraInfoTitle">Humidity:</p> 
-                  <img className="miniIcon"src={humidityIcon} alt="humidity icon"></img>
-                  <p className="extraInfoDescription">{weather.main.humidity}%</p>
-                </div>
-                <div className="extraInfoContainer">
-                  <p className="extraInfoTitle">Pressure:</p> 
-                  <img className="miniIcon"src={pressureIcon} alt="hpa icon"></img>
-                  <p className="extraInfoDescription">{formatPressure(weather.main.pressure)} hPa</p>
-                </div>
-              </div>
-            </div>
           </>
         )}
       </div>
