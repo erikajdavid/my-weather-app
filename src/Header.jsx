40d8eea0771a284161d2dfb8379271f0c86a70ca
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-07 01:38:58 -0400

    revert

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const Header = ({ setCity, searchCity, weather }) => {
+const Header = ({ setCity, searchCity }) => {
 
   return (
     <header>
       <h1>My basic-ass weather app</h1>
       <h2>A real-time international weather app that is nowhere near as complex as the one you're already using.</h2>
-      <SearchBar setCity={setCity} searchCity={searchCity} weather={weather} />
+      <SearchBar setCity={setCity} searchCity={searchCity}/>
     </header>
   )
 }
