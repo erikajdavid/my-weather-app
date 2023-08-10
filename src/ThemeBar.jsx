92061f120427f79fb3057aca89a3c83091a5c8c2
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-09 21:12:22 -0400

    updated styling of themeBar and added animation

--- src/ThemeBar.jsx
+++ src/ThemeBar.jsx
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
+
 const ThemeBar = () => {
-    return (
-        <div className="toggleBar">
-            <i className="fa-solid fa-sun"></i>
-            <i className="fa-solid fa-moon"></i>
-      </div>
-    )
+  const [isSunTheme, setIsSunTheme] = useState(true); // Assuming sun theme is the default
+
+  const toggleTheme = () => {
+    setIsSunTheme(prevTheme => !prevTheme);
+  };
+
+  const sliderPosition = isSunTheme ? '8%' : '50%'; 
+
+  return (
+    <div className="toggleBar">
+      <i className="fa-solid fa-sun" onClick={toggleTheme}></i>
+      <i className="fa-solid fa-moon" onClick={toggleTheme}></i>
+      <div className="slider" style={{ left: sliderPosition }}></div>
+    </div>
+  );
 }
 
 export default ThemeBar;
