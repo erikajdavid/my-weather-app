429befdda73c9d7b52f17396b977b5f9596fcd4c
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-10 21:03:58 -0400

    togggle anywhere on pill button to switch modes

--- src/ThemeBar.jsx
+++ src/ThemeBar.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const ThemeBar = ({ handleToggleTheme, darkMode }) => {
   const sliderPosition = darkMode ? '8%' : '50%';
 
+  const handleThemeClick = () => {
+    const changeTheme = darkMode ? 'day' : 'night';
+    handleToggleTheme(changeTheme);
+  };
+
   return (
-    <section className="modes">
-      <i className="far fa-sun" onClick={() => handleToggleTheme('day')}></i>
-      <i className="fas fa-moon" onClick={() => handleToggleTheme('night')}></i>
+    <section className="modes" onClick={handleThemeClick}>
+      <i className="far fa-sun"></i>
+      <i className="fas fa-moon"></i>
       <div className="slider" style={{ left: sliderPosition }}></div>
     </section>
   );
