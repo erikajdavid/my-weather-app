ba2b5b2033c4e6325acbc7aa995ecad3d00ad3bf
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-08-21 23:07:24 -0400

    signUp styling + routing

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,17 +1,26 @@
+import { Link } from 'react-router-dom';
+
 const SignUp = () => {
     return (
         <div className="signUpPage">
             <div className="formContainer">
                 <h2>Sign Up</h2>
                 <form>
-                    <label htmlFor="text">Email:</label>
-                    <input type="text" required></input>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" required></input>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" required></input>
-                    <label htmlFor="password">Confirm Password:</label>
-                    <input type="password" required></input>
-                    <button type="submit">Sign Up</button>
+                    <div className="passwordContainer">
+                        <input type="password" name="password" className="password" required></input>
+                        <i class="fa-regular fa-eye-slash faInvisible"></i>
+                    </div>
+                    <label htmlFor="confirmPassword">Confirm Password:</label>
+                    <div className="passwordContainer">
+                        <input type="password" name="confirmPassword" className="password" required></input>
+                        <i class="fa-regular fa-eye-slash faInvisible"></i>
+                    </div>
+                    <button type="submit">Get Started</button>
                 </form>
+                <p>Already have an account? <Link to="/login">Log in.</Link></p>
             </div>
         </div>
     )
