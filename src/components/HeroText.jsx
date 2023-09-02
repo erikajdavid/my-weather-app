d717271be7d75be38eb41e526cd3c03c07330f77
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-09-02 17:06:22 -0400

    lil housekeeping

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -6,7 +6,7 @@ const HeroText = ({ setCity, searchCity, weather }) => {
   return (
     <div className="heroText">
       <h1>My basic-ass weather app</h1>
-      <h2>A real-time international weather app that is nowhere near as complex as the one you're already using.</h2>
+      <h2>A real-time global weather app that is nowhere near as complex as the one you're already using.</h2>
       <SearchBar setCity={setCity} searchCity={searchCity} weather={weather}/>
     </div>
   )
